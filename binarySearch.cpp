0d9a7dcdb85d924999de72ecff62c50e0b3f85a7
salil sharma <salilsharma562@gmail.com>
2022-02-04 22:17:22 +0530

    made binarySearchMoreReadble

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,25 +1,28 @@
 #include<iostream>
 using namespace std;
 
-void binarySearch(int,int [],int,int,int);
+int binarySearch(int,int [],int,int,int);
 int main(){
 int arr[]={3,20,22,23,33}; 
+int even[]={44,45};
 int size;
-size=(sizeof(arr)/sizeof(int));
+size=(sizeof(even)/sizeof(int));
 int start=0;
 int end=size-1;
 int find;
 cout<<"Enter the value to find: ";
 cin>>find;
-binarySearch(find,arr,start,end,size);
+int ans;
+ans=binarySearch(find,even,start,end,size);
+cout<<"The index in which element is present is: "<<ans<<endl;
 return 0;
 }
-void binarySearch(int find,int arr[],int start,int end,int n){
-int mid;    
-for(int i=0;i<n;i++){
+int binarySearch(int find,int arr[],int start,int end,int n){
+int mid;   
 mid=(start+end)/2;
+while(start<=end){
 if(arr[mid]==find){
-    break;
+    return mid;
 }
 else if(arr[mid]>find){
 end=mid-1;
@@ -27,7 +30,7 @@ end=mid-1;
 else if(arr[mid]<find){
 start=mid+1;    
 }
+mid=(start+end)/2;
 }
-cout<<"The index we searched is: "<<mid<<endl;
-cout<<"The element present in it is: "<<arr[mid]<<endl;
+return -1;
 }
